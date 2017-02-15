a04eaf77bb56c49bc7a1b321ae22a03e017d13e8
erberger22 <erberger22@gmail.com>
2017-02-15 12:06:44 -0800

    change age answer to integer, update questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,7 +2,8 @@ puts "what is your name?"
 name_applicant=gets.chomp
 puts "how old are you?"
 age_applicant=gets.chomp
-puts "would you like some garlic bread from the cafeteria?"
+
+puts "would you like some garlic bread from the cafeteria? (yes/no)"
 garlic_preference=gets.chomp
-puts "will you be enrolling in our company health plan?"
+puts "will you be enrolling in our company health plan? (yes/no)"
 health_applicant=gets.chomp
