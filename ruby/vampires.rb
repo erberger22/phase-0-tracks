a9a7a8b787007e06213462fa28e6ee2be7f6631b
erberger22 <erberger22@gmail.com>
2017-02-15 12:01:32 -0800

    add questions, assign variables to answers

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "what is your name?"
+name_applicant=gets.chomp
+puts "how old are you?"
+age_applicant=gets.chomp
+puts "would you like some garlic bread from the cafeteria?"
+garlic_preference=gets.chomp
+puts "will you be enrolling in our company health plan?"
+health_applicant=gets.chomp
