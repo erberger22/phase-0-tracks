a8584aab98c1a56cfb2eecd0b162fcd469cabab2
erberger22 <erberger22@gmail.com>
2017-02-15 13:18:39 -0800

    add logic to determine likelyhood of being vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,3 +10,26 @@ puts "would you like some garlic bread from the cafeteria? (yes/no)"
 garlic_preference=gets.chomp
 puts "will you be enrolling in our company health plan? (yes/no)"
 health_applicant=gets.chomp
+
+puts "You said your name is #{name_applicant}, you said you were born in #{year_born} and are #{age_applicant} years old. 
+When offered garlic bread you said #{garlic_preference} and when offered healthcare you said #{garlic_preference}.
+Because of your answers, we can say that:"
+
+case 
+
+when name_applicant==("Drake Cula"||"Tu Fang")
+	puts "you definetly a vampire"	
+
+when age_applicant==(2017-year_born)&&(garlic_preference=="yes"||health_applicant=="yes")
+	puts "you are probably not a vampire." 
+
+when age_applicant!=(2017-year_born)&&(garlic_preference=="no"&&health_applicant=="no")
+	puts "you are almost certainly a vampire."
+
+when age_applicant!=(2017-year_born)&& (garlic_preference=="no"||health_applicant=="no")
+	puts "you are probably a vampire."
+
+else
+	puts "the results are inconclusive"
+end
+
