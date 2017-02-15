e9580ac273199b3c332553bb198dd4e98d373936
erberger22 <erberger22@gmail.com>
2017-02-15 12:09:37 -0800

    add year born question

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,11 @@
 puts "what is your name?"
 name_applicant=gets.chomp
-puts "how old are you?"
+puts "how old are you? (in years)"
 age_applicant=gets.chomp
-
+age_applicant=age_applicant.to_i
+puts "what year were you born?"
+year_born=gets.chomp
+year_born=year_born.to_i
 puts "would you like some garlic bread from the cafeteria? (yes/no)"
 garlic_preference=gets.chomp
 puts "will you be enrolling in our company health plan? (yes/no)"
