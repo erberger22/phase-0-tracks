b222de7f7a8997e4ae18e32a421ed050d11ce092
erberger22 <erberger22@gmail.com>
2017-02-16 13:37:37 -0800

    add pseudocode in comment

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,15 @@
+=begin
+-encrypt method
+	-read imput string
+	-index each character
+	-add 1 to each indexed character(use next method)
+	-save as new encrypted string
+	-puts new encrypted string
+-decryption method
+	-read imput of encrypted string
+	-index each character
+	-subtract 1 from each indexed character (using alphabet reference string)
+	-save as decrypted string
+	-puts decrypted string
+profit
+=end
