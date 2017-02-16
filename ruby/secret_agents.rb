76a8b4996ed3d543c215b65089a5fb7da6269f13
erberger22 <erberger22@gmail.com>
2017-02-16 14:23:35 -0800

    add user interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -24,6 +24,7 @@ def encrypt (password)
 		encrypted_password += encrypted_letter
 		index += 1
 	end
+	return encrypted_password
 	#p encrypted_password
 end
 
@@ -37,14 +38,40 @@ def decrypt (encrypted_password)
 		decrypted_password += key[decrypted_index]
 		index += 1
 	end
+	return decrypted_password
 	#p decrypted_password
 end
 
 #p decrypt(encrypt("swordfish"))
 =begin This nested method worked because it used order of operations to solve for the
-encrypt method within the () first and then called the decrypt method on the encrypted 
-result of encrypt method 
+encrypt method within the () first and then called the decrypt method on the 
+encrypted result of encrypt method 
 =end
+
+=begin 
+-Ask user whether encrypting or decrypting
+-Ask for password
+-If user wants to encrypt
+	-call encrypt method on password
+-If user wants to decrypt
+	-call decrypt method on password
+-print result
+=end
+puts "Will you be encrypting or decrypting a message today, Mr. Bond?
+Enter \"1\" for encrypt and \"2\" for decrypt"
+user_preference=gets.chomp
+puts "Please enter password now"
+user_password=gets.chomp
+if user_preference=="1"
+	puts encrypt(user_password)
+elsif user_preference=="2"
+	puts decrypt(user_password)
+else
+	puts "THIS CONSOLE WILL SELF DESTRUCT IN 5 SECONDS"
+end
+		
+
+
 
 
 
