d25b8957dc8795f22fb698de9f1e57022b45c25b
erberger22 <erberger22@gmail.com>
2017-02-21 13:16:19 -0800

    add pseudocode and empty hash keys

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+=begin 
+-build hash with empty keys for name, age, number of children
+decor theme, allergies, payment method
+-prompt user for imput
+	-enter user data into proper key
+-print hash
+-ask if data is correct
+	- if correct user enters "none" to procede
+	- if key needs revision user enters key 
+		-updates key value
+-print hash
+=end
+
+client ={
+	name: , 
+	age: ,
+	number_of_children: ,
+	decor_theme: ,
+	allergies: [], 
+	payment_method: 
+}
