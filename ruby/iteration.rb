b7ea2b687bd2eb11b34001ee7a8875f6c56124bd
erberger22 <erberger22@gmail.com>
2017-02-22 14:45:37 -0800

    add other array and hash methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,7 @@
+
+=begin
+#Release 0
+
 def farwell
 	puts "Bye-bye"
 	yield ("alligator")
@@ -5,6 +9,8 @@ def farwell
 end
 
 farwell { |phrase| puts "see you later, #{phrase}" }
+
+#Release 1
 
 birds = ["ostrich", "emuu", "penguin", "kiwi"]
 
@@ -29,4 +35,59 @@ bears.each do |bear, is_scary|
 end
 
 p bears
+
+=end
+#Release 2
+
+array_num = [2, 55, 3, 2, 67, 34]
+
+hash_words = {"yellow"=> "adj", "run" => "vrb", "loud" => "adj", "swim" => "vrb"}
+
+p array_num
+array_num.delete_if { |number| number > 30 }
+p array_num
+
+p hash_words
+hash_words.delete_if { |word, word_type| word_type == "adj"}
+p hash_words
+
+array_num = [2, 55, 3, 2, 67, 34]
+
+hash_words = {"yellow"=> "adj", "run" => "vrb", "loud" => "adj", "swim" => "vrb"}
+
+p array_num
+array_num.keep_if { |number| number > 30 }
+p array_num
+
+p hash_words
+hash_words.keep_if { |word, word_type| word_type == "adj"}
+p hash_words
+
+array_num = [2, 55, 3, 2, 67, 34]
+
+hash_words = {"yellow"=> "adj", "run" => "vrb", "loud" => "adj", "swim" => "vrb"}
+
+p array_num
+p array_num.fetch(2)
+p array_num
+
+p hash_words
+p hash_words.assoc("swim")
+p hash_words
+
+array_num = [2, 55, 3, 2, 67, 34]
+
+hash_words = {"yellow"=> "adj", "run" => "vrb", "loud" => "adj", "swim" => "vrb"}
+
+p array_num
+new_array = array_num.take_while { |number| number < 60 }
+p array_num
+p new_array
+
+p hash_words
+new_hash = hash_words.reject { |word, word_type| word_type == "adj"}
+p hash_words
+p new_hash
+
+
 
