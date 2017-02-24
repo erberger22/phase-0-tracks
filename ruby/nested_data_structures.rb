b1979ed0bebc7f3e2726d3ef1e4313feb71b383e
erberger22 <erberger22@gmail.com>
2017-02-24 00:33:24 -0800

    add values to REI key, add missing commas between array and hash items

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,35 +1,67 @@
 mall = {
 	orange_julius:{
-		food:[]
+		food:[],
 		drink:[
 			"tripple berry",
 			"mango pineapple",
 			"orange"
-		]
+		],
 		clothing:{
 			shirt: "t-shirt",
 			hat: "baseball cap"
 		}
-	}
+	},
 	cinnabun:{
 		food:[
 			"Classic Roll",
 			"Minibon",
 			"Cinnabun Bites"
-		]
+		],
 		drink:[
 			"Soft Drink",
 			"Vanilla Iced Coffee",
 			"Signature Coffee"
-		]
+		],
 		clothing:{}
 
-	}
+	},
 	REI:{
-
-	}
-	gap:{
-
-	}
+		food: [
+			"Cliff Bar",
+			"Dried Apricots",
+			"Buffalo Jerky"
+		],
+		drink:[],
+		clothing:{
+			shirt:[
+				"casual",
+				"running",
+				"travel",
+				"yoga",
+				"rashguard"
+			],
+			pants:[
+				"quick dry",
+				"cargo",
+				"convertible"
+			],
+			hat:[
+				"beanie",
+				"earflap",
+				"safari"
+			]
 		}
+	},
+	gap:{
+		food:[],
+		drink:[],
+		clothing:{
+		}
+	}
 }
+
+p mall
+
+
+
+
