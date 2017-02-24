b1bff5d94f049dd059ea3a4c5772a8713f5867f4
erberger22 <erberger22@gmail.com>
2017-02-24 00:14:44 -0800

    create mall has, add keys, add key values to orange julius key

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,24 @@
+mall = {
+	"orange julius":{
+		food:[]
+		drink:[
+			"tripple berry",
+			"mango pineapple",
+			"orange"
+		]
+		clothing:{
+			shirt: "t-shirt",
+			hat: "baseball cap"
+		}
+	}
+	"cinnabun":{
+
+	}
+	"REI":{
+
+	}
+	"gap":{
+
+	}
+		}
+}
