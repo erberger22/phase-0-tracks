489f6d7ec76432fc4c8b6e83473ad3ce6b2fa890
erberger22 <erberger22@gmail.com>
2017-02-27 10:00:38 -0800

    add design for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+=begin 
+SPECIES
+ Puppy
+
+CHARACTERISTICS
+ Name: varies
+ Color: varies
+ Leg count: 3-4
+ Tail count: 0-1
+ Cute: yes
+ Friendly: yes
+ Potty trained: no
+
+BEHAVIOR
+ jump
+ pant
+ eat
+ sleep
+ cuddle
+=end
+
+
