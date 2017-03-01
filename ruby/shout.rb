229f5180a618be57d72d272affb77b723cf24c83
erberger22 <erberger22@gmail.com>
2017-03-01 10:48:14 -0800

    Add two new mixin methods in the Shout module. Add two classes that include the Shout module and and test it by runnning the mixin methods in each class

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,31 @@
 module Shout
-	def self.yell_angrily(words)
-    words + "!!!" + " :("
+	#def self.yell_angrily(words)
+  #  words + "!!!" + " :("
+  #end
+  #def self.yell_happily(words)
+  #	 words + " :)" + " <3" + " :)"
+  #end
+  def outdoor_voice(words)
+  	puts words.upcase + "!!!"
   end
-  def self.yell_happily(words)
-  	words + " :)" + " <3" + " :)"
+  def indoor_voice(words)
+  	puts words.downcase + "..."
   end
 end
 
-p Shout.yell_angrily("Tacos")
-p Shout.yell_happily("Tacos")
+class Umpire
+	include Shout
+end
+
+class Lifeguard
+	include Shout
+end
+
+home_ump = Umpire.new
+home_ump.outdoor_voice("strike three")
+home_ump.indoor_voice("strike three")
+
+lifeguard = Lifeguard.new
+lifeguard.outdoor_voice("dont run")
+lifeguard.indoor_voice("dont run")
+
