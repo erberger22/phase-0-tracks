6fe8f4fc7e5fce0073e51c5aea54b289caabe884
erberger22 <erberger22@gmail.com>
2017-02-28 19:36:47 -0800

    Add driver code that has 3 different initializaions of new Santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,15 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
+		puts "Ho, ho, ho! Haaaappy holidays from #{@gender} santa!"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
@@ -13,8 +17,32 @@ class Santa
 	end
 end
 
-ron = Santa.new
+#ron = Santa.new
+#ron.speak
+#ron.eat_milk_and_cookies("oatmeal rasin")
 
-ron.speak
-ron.eat_milk_and_cookies("oatmeal rasin")
+#Entering simple initializations
+
+#santa_1 = Santa.new("Bigender", "Cuban")
+#p santa_1
+#santa_2 = Santa.new("Male", "Chinese")
+#p santa_2
+#santa_3 = Santa.new("Female", "Icelandic")
+#p santa_3
+
+#Entering initializations with a loop
+
+santa_gender_and_ethnicity = 
+{"Bigender"=> "Cuban", "Male"=> "Chinese", "Female"=> "Icelandic"}
+santas = []
+
+santa_gender_and_ethnicity.each do |gender, ethnicity|
+	santas << Santa.new(gender, ethnicity)
+end
+santas[2].speak
+p santas
+
+
+
+
 
