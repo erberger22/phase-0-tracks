3b5004d8149bf0eb99d11839439a821cf3f41f10
erberger22 <erberger22@gmail.com>
2017-02-28 21:18:59 -0800

    Add santacon method that takes the desired numper of santas as an argument and prints the attributes of each santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-	attr_reader :age, :ethnicity
-	attr_accessor :gender
+	attr_reader :ethnicity, :reindeer_ranking
+	attr_accessor :gender, :age
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
 		@gender = gender
@@ -27,6 +27,22 @@ class Santa
 	end
 end
 
+#array.sample takes a random element
+def santacon(santa_count)
+number_of_santas = 0
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+	until number_of_santas == santa_count
+		new_santa = Santa.new(genders.sample, ethnicities.sample)
+		new_santa.age = rand(140)
+		puts "This #{new_santa.ethnicity} santa is #{new_santa.age} years old and identifies as #{new_santa.gender}. #{new_santa.reindeer_ranking[0]} is the favorite reindeer and #{new_santa.reindeer_ranking[-1]} is the least favorite."
+		puts "-----"
+		number_of_santas += 1
+	end
+end
+
+santacon(30)
+
 #ron = Santa.new
 #ron.speak
 #ron.eat_milk_and_cookies("oatmeal rasin")
@@ -42,6 +58,7 @@ end
 
 #Entering initializations with a loop
 
+=begin 
 santa_gender_and_ethnicity = 
 {"Bigender"=> "Cuban", "Male"=> "Chinese", "Female"=> "Icelandic"}
 santas = []
@@ -66,6 +83,8 @@ ron.gender = "gender fluid"
 ron.speak
 
 #p santas
+=end 
+
 
 
 
