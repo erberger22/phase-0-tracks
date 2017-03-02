d0c7675f48f0f34cab2691156f3d5d56d66906cc
erberger22 <erberger22@gmail.com>
2017-03-02 14:58:28 -0800

    all tests return as green

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,8 +1,12 @@
 class Hangman_game
+	attr_reader :word
+	attr_accessor :guesses
 	
 	def initialize(word)
 		@word = word
-		@solution = []
+		@guesses = []
+		@solution_array = []
+
 	end
 
 	def show_word
@@ -16,10 +20,42 @@ class Hangman_game
 	def num_guesses
 		@word.length*2
 	end
+	
 	def solution
 		word_array.each do
-		@solution << "_"
+		@solution_array.push"_"
 		end
-		@solution
+		@solution_array
 	end	
+
+	def repeat(user_guess)
+		#return false if user guess is new, return true if user guess repeat
+		@guesses.include?(user_guess)
+	end
+
+	def store_guess(user_guess)
+		@guesses << user_guess
+	end
+
+	def check_guess(user_guess)
+		word_array.include?(user_guess)
+	end
+
+	def insert_answer(user_guess)
+		solution = self.solution
+		counter = 0
+		until counter == @word.length
+			if word_array[counter] == user_guess 
+				solution[counter] = user_guess
+			end
+			counter += 1
+		end
+		solution
+	end
 end
+
+puts "Enter a word to be guessed:"
+word = gets.chomp
+
+
+
--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -19,6 +19,27 @@ describe Hangman_game do
 		expect(game.solution).to eq ["_","_","_","_","_","_"]
 	end
 
+	it "compares user guess to the guess array" do
+		expect(game.repeat("o")).to eq false
+	end
+
+	it "adds user guess to the guess array" do
+		expect(game.store_guess("o")).to eq ["o"]
+	end
+
+	it "checkes if user guess is included in the word" do
+		expect(game.check_guess("o")).to eq true
+	end
+
+	it "inserts the user guess into the correct location, and returns updated solution" do
+		expect(game.insert_answer("o")).to eq ["_","_","_","_","o","_"]
+	end
+
+	it "inserts the user guess into the correct location, and returns updated solution" do
+		expect(game.insert_answer("t")).to eq ["_","_","t","t","_","_"]
+	end
+
+
 
 
 
