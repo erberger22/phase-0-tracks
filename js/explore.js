f01eacaad5770e97de37a18813a10b7fa9247c68
erberger22 <erberger22@gmail.com>
2017-03-06 12:47:17 -0800

    Add reverseString function

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,11 @@
 //Function will print a new string, starting with the -1 position and working backwards until it reaches the -(length of string) value
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length-1; i >= 0; i -= 1){
+    	newString += str[i]
+    }
+    return newString;
+}
+
+console.log(reverseString("hello"))
