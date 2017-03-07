f8b7ab9bc08503610f6159d5b5f1462759b0cb1f
erberger22 <erberger22@gmail.com>
2017-03-07 10:51:11 -0800

    Add color array and horse name array, add objects and test

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+var colors = ["green", "blue", "red", "orange"];
+var horse_name = ["Ed", "Nancey", "Abbey", "Oats"];
+
+console.log(colors);
+console.log(horse_name);
+
+colors.push("pink");
+horse_name.push("Jeff");
+
+console.log(colors);
+console.log(horse_name);
