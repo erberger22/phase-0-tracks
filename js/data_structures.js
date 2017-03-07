81774c1d32f85765a9d31468c5578ed9bbb691b6
erberger22 <erberger22@gmail.com>
2017-03-07 11:24:06 -0800

    Add car constructor function, create new car object and test beep function

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,23 @@ for (var i = 0; i < colors.length; i++) {
 	horses[horse_name[i]] = colors[i]
 }
 console.log(horses)
+
+function Car(color, miles, make){
+	this.color = color;
+	this.miles = miles; 
+	this.make = make;
+
+	this.beep = function(make) {
+		if (this.make == "honda"){
+			return "Honk!";
+		} else {
+			return"Beep!";
+		}
+	}
+}
+
+var newCar = new Car ("blue", 1000, "Dodge")
+console.log(newCar.miles)
+console.log(newCar.beep())
+newCar.make = "honda"
+console.log(newCar.beep())
