22302f712aaf7b5fa3c589f911e8e884bba6c0fd
erberger22 <erberger22@gmail.com>
2017-03-09 14:43:35 -0800

    Add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,3 @@
+//funtion will loop through the array, and store the .length of each string into a new array
+//Call .max on the new array to find which value is the largest
+//the index of the largest value in the new array is the index of the longest string in the origional array
