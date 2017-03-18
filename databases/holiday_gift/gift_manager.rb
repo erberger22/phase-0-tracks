84262ad857d63949a1aa344b32cab62ea4b42021
erberger22 <erberger22@gmail.com>
2017-03-18 15:16:23 -0700

    Changed values of table columns created, created new db

--- databases/holiday_gift/gift_manager.rb
+++ databases/holiday_gift/gift_manager.rb
@@ -3,13 +3,19 @@ require 'sqlite3'
 db = SQLite3::Database.new("holidays.db")
 
 create_table_cmd_1 = <<-SQL
-  CREATE TABLE IF NOT EXISTS familymember(
+  CREATE TABLE IF NOT EXISTS personal_gift(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    age INT,
-    likes_hats BOOLEAN, 
-    gift VARCHAR(255)
+    gift VARCHAR(255),
+    cost INT,
+    purchased BOOLEAN
   )
   SQL
 
 db.execute(create_table_cmd_1)
+
+def add_gift_reciever(db, name, gift, cost, purchased)
+  db.execute("INSERT INTO familymember (name, gift, cost, purchased) VALUES (?, ?, ?, ?)", [name, gift, cost, purchased])
+end
+
+
