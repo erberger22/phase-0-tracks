6cb61ac4b50f11e75a5b8bc4286a71a3a99aef9c
erberger22 <erberger22@gmail.com>
2017-03-18 15:08:21 -0700

    Add create table command, run to create familymember table in holidays.db

--- databases/holiday_gift/gift_manager.rb
+++ databases/holiday_gift/gift_manager.rb
@@ -1,3 +1,15 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new("holidays.db")
+
+create_table_cmd_1 = <<-SQL
+  CREATE TABLE IF NOT EXISTS familymember(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    likes_hats BOOLEAN, 
+    gift VARCHAR(255)
+  )
+  SQL
+
+db.execute(create_table_cmd_1)
